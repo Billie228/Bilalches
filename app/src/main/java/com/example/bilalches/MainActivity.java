56af52cec2ddd1d3d20cf8414cd1a0572364cc63
Bilal <akhrorov.bilal@mail.ru>
2022-12-25 14:30:05 +0300

    Конструктор класса

--- app/src/main/java/com/example/bilalches/MainActivity.java
+++ app/src/main/java/com/example/bilalches/MainActivity.java
@@ -6,9 +6,22 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Cat myCat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myCat = new Cat(4,"Puma","Yaz","gray");
+        myCat.talk();
+
+        Cat murzik = new Cat(2,"Murza","Karas","blue");
+        murzik.talk();
+
+        Cat glasha = new Cat();
+        glasha.age = 1;
+        glasha.name = "Glasha";
     }
 }
