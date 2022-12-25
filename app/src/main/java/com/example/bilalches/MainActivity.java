9ed5663a279881c4b13c8d20c18d59f548df372f
Bilal <akhrorov.bilal@mail.ru>
2022-12-25 14:41:29 +0300

    Полиморфизм

--- app/src/main/java/com/example/bilalches/MainActivity.java
+++ app/src/main/java/com/example/bilalches/MainActivity.java
@@ -16,41 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myCat = new Cat(4, "Puma", "Yaz", "gray");
-        myCat.talk();
-
-        Cat murzik = new Cat(2, "Murza", "Karas", "blue");
-        murzik.talk();
-
-        Cat glasha = new Cat();
-        glasha.age = 1;
-        glasha.name = "Glasha";
-        glasha.breed = "Buba";
-        glasha.color = "black";
-        glasha.talk();
-
         Cat vaska = new Cat();
         vaska.talk("Meow, meow!");
 
-        Puma puma = new Puma();
-        puma.name = "Leo";
+        Puma bagira = new Puma();
+        bagira.talk();
 
-        puma.breathe();
+        Cat puma = new Cat();
         puma.talk();
-        puma.isAlive = true;
-        Log.i("isAlive", "My name is " + puma.name + " and " + puma.breathe());
-    }
 
-    void method(double x, int y) {
-        int a;
-        a = 1;
-        a = y + 1;
-        double d = x * 2;
-        z = a + y;
-    }
-
-    void method1() {
-        int a;
-        a = 1;
+        Cat murzik = new Lion();
+        murzik.talk();
     }
 }
